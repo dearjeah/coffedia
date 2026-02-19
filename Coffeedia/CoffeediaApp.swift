96b8fdf426bbff5e022f66cda3267e3b10aa52bb
del <j.delvina@gmail.com>
2026-02-19 10:40:40 +0700

    Initial Commit

---
+++ Coffeedia/CoffeediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeediaApp.swift
+//  Coffeedia
+//
+//  Created by Delvina J on 19/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CoffeediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
